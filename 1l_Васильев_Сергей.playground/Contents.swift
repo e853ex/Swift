b0cfcef3fdb4de339f005cbcc485e9351001f88d
Sergey <e853ex@yandex.ru>
2020-09-23 12:46:24 +0300

    Исправил

--- 1l_Васильев_Сергей.playground/Contents.swift
+++ 1l_Васильев_Сергей.playground/Contents.swift
@@ -1,17 +1,23 @@
 import Foundation
 
-//Задание 1
+//Задание 1  - Переделал
+let equation = "(ax^2)-8x+15=0"
+let a = 1.0
+let b = -8.0
+let c = 15.0
 
-let a = 6.0
-let b = 8.0
-let c = 128.0
-let x = 4.0
-let sum = 0
+let dis: Double = sqrt((b*b)-4*a*c)
 
-let answer = (Int)((a * pow(x, 2)) + (b * x) - c)
-
-print("\(answer) = \(sum)")
-
+if dis == 0 {
+    let radical: Double = (b-1) / 2*a
+    print("У уравнения \(equation) один корень и он равен \(radical)")
+} else if dis < 0 {
+    print("У уравнения \(equation) корней нет")
+} else {
+    let rad1: Double = (b-1) - dis / (2*a)
+    let rad2: Double = (b-1) + dis / (2*a)
+    print("Уравнение \(equation) имеет два корня \(rad1) и \(rad2)")
+}
 
 //Задание 2
 
@@ -21,7 +27,7 @@ let r = 4.0
 
 let q1 = pow(q, 2)
 let w1 = pow(w, 2)
-let hypotenuse = q1 + w1
+var hypotenuse:Double = sqrt(q1 + w1) // Исправил
 
 print("Гипотенуза равна: \(hypotenuse)")
 
